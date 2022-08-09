f03cc93b6cbe901296c735c12c2eaffaa7a8a34e
forska <736523388@qq.com>
2022-08-09 15:42:51 +0800

    博客详情增加图片

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,29 +1,44 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../../components/layout'
+import * as React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../../components/layout";
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.image_path);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>
-        {data.mdx.body}
-      </MDXRenderer>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.image_credit_link}>
+          {data.mdx.frontmatter.image_credit_text}
+        </a>
+      </p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        image_alt
+        image_credit_link
+        image_credit_text
+        image_path {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
-`
+`;
 
-export default BlogPost
+export default BlogPost;
